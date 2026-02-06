bc71f5fde6a6e2d3ba08f3891039db6aaf03774e
Abhishek26696 <bangariabhishek889@gmail.com>
2026-02-06 14:45:07 +0530

    Initial commit of C program

---
+++ world.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>   // for sleep()
+
+// CRC-8 function (polynomial 0x07)
+unsigned char crc8(unsigned char *data, int len)
+{
+    unsigned char crc = 0x00;
+    unsigned char poly = 0x07;
+
+    for (int i = 0; i < len; i++)
+    {
+        crc ^= data[i];
+
+        for (int j = 0; j < 8; j++)
+        {
+            if (crc & 0x80)
+                crc = (crc << 1) ^ poly;
+            else
+                crc <<= 1;
+        }
+    }
+    return crc;
+}
+
+int main()
+{
+    unsigned char sof = 0xFF;
+    unsigned char payload[8];
+    unsigned char crc;
+
+    // Seed random number generator
+    srand(time(NULL));
+
+    while (1)
+    {
+        // Generate random 8-byte payload
+        for (int i = 0; i < 8; i++)
+        {
+            payload[i] = rand() % 256;
+        }
+
+        // Calculate CRC on payload
+        crc = crc8(payload, 8);
+
+        // Print frame (SOF + DATA + CRC)
+        printf("%02X ", sof);
+
+        for (int i = 0; i < 8; i++)
+        {
+            printf("%02X ", payload[i]);
+        }
+
+        printf("%02X\n", crc);
+
+        // 2 second delay → 0.5 Hz
+        sleep(2);
+    }
+
+    return 0;
+}
